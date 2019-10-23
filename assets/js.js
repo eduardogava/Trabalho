5f148d5c1141bea84100c8bd70f4c82acc1adef4
eduardogava <55919893+eduardogava@users.noreply.github.com>
2019-10-23 13:50:30 -0300

    dsa
    dsa

---
+++ assets/js.js
@@ -0,0 +1,15 @@
+functionsupportsPlaceholder() {
+    return 'placeholder' in document.createElement('input') &&
+        'placeholder' in document.createElement('textarea');
+}
+
+jQuery(function ($) {
+    functionsupportsPlaceholder() {
+        return 'placeholder' in document.createElement('input') &&
+            'placeholder' in document.createElement('textarea');
+    }
+
+    if (!supportsPlaceholder()) {
+        $('html').addClass('no-placeholder');
+    }
+});
