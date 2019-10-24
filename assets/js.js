5b1fbd75546dd039d99c8ff342fa1578133d6b25
eduardogava <55919893+eduardogava@users.noreply.github.com>
2019-10-24 16:11:25 -0300

    Trabalho
    dsa

--- assets/js.js
+++ assets/js.js
@@ -1,15 +1,14 @@
-functionsupportsPlaceholder() {
+function supportsPlaceholder(){
     return 'placeholder' in document.createElement('input') &&
-        'placeholder' in document.createElement('textarea');
-}
+     'placeholder' in document.createElement('textarea'); 
+   }
 
-jQuery(function ($) {
-    functionsupportsPlaceholder() {
-        return 'placeholder' in document.createElement('input') &&
-            'placeholder' in document.createElement('textarea');
-    }
-
-    if (!supportsPlaceholder()) {
-        $('html').addClass('no-placeholder');
-    }
+jQuery(function($) {
+   function supportsPlaceholder() {
+       return'placeholder'in document.createElement('input')  &&
+             'placeholder'in document.createElement('textarea');
+   }
+   if(!supportsPlaceholder()) {
+       $('html').addClass('no-placeholder');
+   }
 });
